7ed4972cdfed3fc8ce668f332b1019059cd79ce3
Chenchen Guo <gcc9108@gmail.com>
2016-07-08 14:02:04 -0700

    Fixed relative path issue. Execute 'npm start' at the root directory of the project to start the server side program.

--- server/server.js
+++ server/server.js
@@ -14,11 +14,11 @@ app.use(bodyParser.json());
 		images -> assets/
 	(eg. 'javascript/home.js')
 */
-app.use(express.static('../client'));
-app.use(express.static('../client/html'));
-app.use('/js', express.static('../node_modules/bootstrap/dist/js'));		// redirect bootstrap JS
-app.use('/js', express.static('../node_modules/jquery/dist'));				// redirect JS jQuery
-app.use('/css', express.static('../node_modules/bootstrap/dist/css'));		// redirect CSS bootstrap
+app.use(express.static('client'));
+app.use(express.static('client/html'));
+app.use('/js', express.static('node_modules/bootstrap/dist/js'));		// redirect bootstrap JS
+app.use('/js', express.static('node_modules/jquery/dist'));				// redirect JS jQuery
+app.use('/css', express.static('node_modules/bootstrap/dist/css'));		// redirect CSS bootstrap
 
 // redirect empty url to home page
 app.get("/", function(req, res) {
